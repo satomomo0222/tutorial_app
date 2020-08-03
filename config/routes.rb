bea841ede43a700eebbcf61bc368620721b61052
satomomo0222 <satomomo0222@gmail.com>
2020-08-03 14:13:52 +0900

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
-  get '/about',to: 'static_pages#about'
   get '/help',to: 'static_pages#help'
+  get '/about',to: 'static_pages#about'
   get '/contact',to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
   get '/login', to: 'sessions#new'
@@ -11,4 +14,5 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
