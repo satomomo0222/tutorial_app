11ea3dc3a51c2b3b167a716301315c8ebed301f6
satomomo0222 <satomomo0222@gmail.com>
2020-08-05 16:22:51 +0900

    Add user microposts

--- Gemfile
+++ Gemfile
@@ -62,3 +62,10 @@ gem 'jquery-ui-rails'
 gem 'faker', '1.7.3'
 gem 'will_paginate', '3.1.7'
 gem 'bootstrap-will_paginate', '1.0.0'
+
+gem 'carrierwave',             '1.2.2'
+gem 'mini_magick',             '4.7.0'
+
+group :production do
+  gem 'fog', '1.42'
+  end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new  
@@ -53,21 +54,18 @@ class UsersController < ApplicationController
                                   :password_confirmation)
     end
 
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-
+    # beforeフィルター
+    
+    # 正しいユーザーかどうかを確認
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
+    
+    # 管理者かどうかを確認
 
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
+
 end
